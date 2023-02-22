ae3d4f5fc03c0763caacfd50d641dd8e54ff668c
blase147 <solarmails2@gmail.com>
2023-02-22 15:55:01 +0100

    Fix NoMethod error in Device

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
 
-  validates :name, presence: true
-  validates :PostsCounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   def admin?
     role == 'admin'
   end
