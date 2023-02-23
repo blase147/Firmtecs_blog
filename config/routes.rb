dee928587485d0b015e92ac903adfa8decb9c4b6
blase147 <solarmails2@gmail.com>
2023-02-23 14:39:40 +0100

    Add API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,19 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-    resources :comments, only: [:new, :create, :destroy]
-    resources :likes, only: [:create]
+      resources :comments, only: [:new, :create, :destroy]
+      resources :likes, only: [:create]
+    end
   end
-end
+
+  namespace :api do
+    post 'login', to: 'login#index'
+    resources :users, only: [] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
